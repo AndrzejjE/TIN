061013995309daad755e126db928e3b05f3779b7
AndrzejjE <93799346+AndrzejjE@users.noreply.github.com>
2023-01-14 00:03:36 +0100

    Main

---
+++ Zad2.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+  let numbers = input.split(' ');
+  let num1 = parseInt(numbers[0]);
+  let num2 = parseInt(numbers[1]);
+  console.log(`${num1 + num2}`);
+});
+
